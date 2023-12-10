8b096f534ecb6a89dac2897b11c1a2c1bad49c5b
Rishat23 <rishatyaparov23@gmail.com>
2023-12-10 19:43:17 +0500

    Поправил routs

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,8 +3,6 @@ import ContactsPage from '@pages/contacts-page'
 import ExtraInformationPage from '@pages/extra-information-page/extra-information-page.jsx'
 import GoodDetailPage from '@pages/good-detail-page'
 import Main from '@pages/main-page'
-import PaymentPage from '@pages/payment-page/index.js'
-import ReturnPage from '@pages/return-page/index.js'
 import StockPage from '@pages/stock-page/stock-page.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
@@ -21,15 +19,6 @@ function Routes() {
 		{
 			path: `/contacts`,
 			element: <ContactsPage />
-		},
-		{
-
-			path: `/payment/:id`,
-			element: <PaymentPage />
-		},
-		{
-			path: `/return/:id`,
-			element: <ReturnPage />
 		},
 		{
 			path: `/stock`,
