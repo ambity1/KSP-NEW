9bbae2ab4f21a7e2c60ca7a59203069e6a078d9a
sl1dee <118618836+sl1dee@users.noreply.github.com>
2023-12-14 22:34:10 +0500

    внесение компонентов с consta uikit в подбор запчастей, поровнял блок к слайдеру

--- src/components/main/hero-screen/selection/by-vin/by-vin.jsx
+++ src/components/main/hero-screen/selection/by-vin/by-vin.jsx
@@ -2,44 +2,75 @@ import { Select } from '@consta/uikit/Select'
 import { useState } from 'react'
 
 import cl from './by-vin.module.scss'
+import './by-vin.scss'
 
 const ByVin = () => {
-	const [value, setValue] = useState()
+	const itemsVin = [
+		{
+			label: '123',
+			id: 1
+		},
+		{
+			label: '456',
+			id: 2
+		},
+		{
+			label: '789',
+			id: 3
+		}
+	]
+	const [vin, setVin] = useState(itemsVin)
+
+	const itemsParts = [
+		{
+			label: 'Крыло',
+			id: 1
+		},
+		{
+			label: 'Капот',
+			id: 2
+		},
+		{
+			label: 'Фара',
+			id: 3
+		}
+	]
+	const [parts, setParts] = useState(itemsParts)
 
 	return (
-		<>
-			<div className={cl.wrapper}>
+		<div className={cl.wrapper}>
+			<div className={cl.selectorBlock}>
 				<span className={cl.title}>VIN номер</span>
 				<Select
 					placeholder="Выберите значение"
-					items={[]}
+					items={itemsVin}
 					size="m"
-					label="Select"
-					caption="Пример"
-					labelPosition="top"
+					// label="Select"
+					// caption="Пример"
+					// labelPosition="top"
 					form="default"
 					view="default"
-					value={value}
-					onChange={({ value }) => setValue(value)}
+					value={vin}
+					onChange={({ value }) => setVin(value)}
 				/>
 			</div>
 
-			<div className={cl.wrapper}>
+			<div className={cl.selectorBlock}>
 				<span className={cl.title}>Название запчасти</span>
 				<Select
 					placeholder="Выберите значение"
-					items={[]}
+					items={itemsParts}
 					size="m"
-					label="Select"
-					caption="Пример"
+					// label="Select"
+					// caption="Пример"
 					labelPosition="top"
 					form="default"
 					view="default"
-					value={value}
-					onChange={({ value }) => setValue(value)}
+					value={parts}
+					onChange={({ value }) => setParts(value)}
 				/>
 			</div>
-		</>
+		</div>
 	)
 }
 
