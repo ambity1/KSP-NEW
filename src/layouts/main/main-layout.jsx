2642c96e8e55fac2e5610fee4861d3adf776e732
sl1dee <118618836+sl1dee@users.noreply.github.com>
2024-01-19 13:29:02 +0500

    исправил ошибку в main-layout.jsx

--- src/layouts/main/main-layout.jsx
+++ src/layouts/main/main-layout.jsx
@@ -1,9 +1,6 @@
 import { Theme, presetGpnDark } from '@consta/uikit/Theme'
-
 import { bgColors } from '@consta/uikit/__internal__/src/uiKit/components/ThemePreview/helpers.js'
-
 import useScrollTop from '@hooks/use-scroll-top.js'
-
 import { memo } from 'react'
 
 import Footer from '@components/footer'
@@ -14,14 +11,13 @@ import BreadCrumbs from '../components/bread-сrumbs'
 const MainLayout = ({ children, hideBreadCrumbs }) => {
 	useScrollTop()
 	return (
-		<>
 		<Theme preset={presetGpnDark}>
 			<Header />
 			{!hideBreadCrumbs && <BreadCrumbs />}
 			{children}
 
 			<Footer />
-		</>
+		</Theme>
 	)
 }
 
