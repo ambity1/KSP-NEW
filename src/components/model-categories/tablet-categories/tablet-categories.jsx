4d720a1a1911ff5fd5fe1631756e2e097648870b
sl1dee <118618836+sl1dee@users.noreply.github.com>
2024-01-31 17:59:02 +0500

    добавил новые файлы на категорий

---
+++ src/components/model-categories/tablet-categories/tablet-categories.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import cl from './tablet-categories.module.scss'
+
+const TabletCategories = () => {
+	return (
+		<div>
+			<div>123</div>
+		</div>
+	)
+}
+
+export default TabletCategories
